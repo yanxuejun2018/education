cef8a83c5bb26b2114f322d537665271929d6e98
yanxuejun2018 <1181977746@qq.com>
2019-05-23 15:07:22 +0800

    'flor'

--- src/Pages/Course/CourseList.jsx
+++ src/Pages/Course/CourseList.jsx
@@ -6,19 +6,7 @@ const IMG_PRE = 'http://localhost:1688/uploads/';
 class CourseList extends Component {
   render() {
     const { sourceData } = this.props;
-    // console.log(sourceData);
-    /*
-       course_intro: "全程案例驱动教学！"
-        course_name: "HTML+CSS"
-        course_page: "upload_0c1d4d95c0f6f7098ddab7d5d595c755.jpg"
-        course_serialize_status: "更新中"
-        course_sub_title: "全程案例驱动"
-        course_tag: "html,css"
-        course_teacher: "撩大"
-        course_title: "html+css案例实战"
-        main_category: "Web大前端"
-        sub_category: "html+css"
-    */
+
     return (
       <div className="container-fluid">
         <div className="body course-list">
